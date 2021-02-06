af0ddf0e1e43c9c5de673b97019123ec9c6da86d
Aubi <aubiergesohou@gmail.com>
2021-02-06 00:08:06 -0500

    End main view(handle clic events)

--- tp1/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ tp1/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
+        Button playButton = findViewById(R.id.play_button);
+        Button aboutButton = findViewById(R.id.about_button);
+        Button exitButton = findViewById(R.id.exit_button);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gameAct = new Intent(MainActivity.this, gameActivity.class);
+                startActivity(gameAct);
+            }
+        });
+
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gameAct = new Intent(MainActivity.this, aboutActivity.class);
+                startActivity(gameAct);
+            }
+        });
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               finish();
+            }
+        });
     }
 }
