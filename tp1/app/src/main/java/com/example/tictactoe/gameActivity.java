43bf291e27c18455402f6009be9f176196d1e210
Aubi <aubiergesohou@gmail.com>
2021-02-08 21:21:21 -0500

    Refactor

--- tp1/app/src/main/java/com/example/tictactoe/gameActivity.java
+++ tp1/app/src/main/java/com/example/tictactoe/gameActivity.java
@@ -12,11 +12,13 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class gameActivity extends AppCompatActivity {
     GridLayout gridLayout;
     TextView[][] textViews;
     GameInfoSingleton gameInfos;
+    String emptyGrid = "P";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,54 +28,63 @@ public class gameActivity extends AppCompatActivity {
         gameInfos = GameInfoSingleton.getInstance();
         setUpGrid();
     }
+
+    void setUpTextViews(int size){
+        textViews = new TextView[size][size];
+
+        Typeface face = ResourcesCompat.getFont(this, R.font.baloo);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                textViews[i][j] = new TextView(gameActivity.this);
+                textViews[i][j].setText(emptyGrid);
+                textViews[i][j].setTypeface(face);
+                textViews[i][j].setTextSize(45);
+                textViews[i][j].setTextColor(getResources().getColor(R.color.white));
+                textViews[i][j].setPadding(40, 40, 40, 40);
+                textViews[i][j].setGravity(Gravity.CENTER);
+                textViews[i][j].setBackground(getResources().getDrawable(R.drawable.grid_border));
+                gridLayout.addView(textViews[i][j]);
+
+                setOnTextViewClickActions(textViews[i][j]);
+            }
+        }
+
+    }
+
+    void setOnTextViewClickActions(TextView textView){
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (textView.getText() != emptyGrid) {
+                    Toast.makeText(getBaseContext(), "This position is already taken", Toast.LENGTH_SHORT).show();
+                } else {
+                    textView.setText(gameInfos.getCurrentPlayer());
+                    TextView currentPlayerView = findViewById(R.id.currentPlayerView);
+
+                    if (gameInfos.getCurrentPlayer() == Player.player1.getValue()) {
+                        textView.setTextColor(getResources().getColor(R.color.palette_pink));
+                        currentPlayerView.setText("Player 1 turn");
+
+                    } else {
+                        textView.setTextColor(getResources().getColor(R.color.palette_blue));
+                        currentPlayerView.setText("Player 2 turn");
+                    }
+                    gameInfos.setNextPlayer();
+                }
+            }
+        });
+    }
+
     void setUpGrid() {
         int size = this.gameInfos.getGridSize();
-        textViews = new TextView[size][size];
 
         gridLayout = findViewById(R.id.grid);
         gridLayout.setOrientation(GridLayout.HORIZONTAL);
         gridLayout.setColumnCount(size);
         gridLayout.setRowCount(size);
 
+        setUpTextViews(size);
 
-        Typeface face = ResourcesCompat.getFont(this, R.font.baloo);
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-
-                textViews[i][j] = new TextView(gameActivity.this);
-                textViews[i][j].setText("O");
-                textViews[i][j].setTypeface(face);
-                textViews[i][j].setTextSize(60);
-                textViews[i][j].setTextColor(getResources().getColor(R.color.white));
-                textViews[i][j].setPadding(60, 60, 60, 60);
-                textViews[i][j].setGravity(Gravity.CENTER);
-                textViews[i][j].setBackground(getResources().getDrawable(R.drawable.grid_border));
-                gridLayout.addView(textViews[i][j]);
-            }
-        }
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                TextView textView = textViews[i][j];
-
-                textView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        textView.setText(gameInfos.getCurrentPlayer());
-                        TextView currentPlayerView = findViewById(R.id.currentPlayerView);
-
-                        if (gameInfos.getCurrentPlayer() == Player.player1.getValue()) {
-                            textView.setTextColor(getResources().getColor(R.color.palette_pink));
-                            currentPlayerView.setText("Player 1 turn");
-
-                        } else {
-                            textView.setTextColor(getResources().getColor(R.color.palette_blue));
-                            currentPlayerView.setText("Player 2 turn");
-                        }
-                        gameInfos.setNextPlayer();
-                    }
-                });
-            }
-        }
     }
 }
