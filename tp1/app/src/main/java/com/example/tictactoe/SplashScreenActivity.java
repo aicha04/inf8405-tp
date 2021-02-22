d6184446d87f579a7dbc96f388e8db705ddc17b6
aicha <miloudi.ai04@gmail.com>
2021-02-22 18:56:09 -0500

    went back to animation and changed colors of buttons

--- tp1/app/src/main/java/com/example/tictactoe/SplashScreenActivity.java
+++ tp1/app/src/main/java/com/example/tictactoe/SplashScreenActivity.java
@@ -8,7 +8,6 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
-import android.widget.VideoView;
 
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -17,32 +16,17 @@ public class SplashScreenActivity extends AppCompatActivity {
     int TIME_INTERVAL = 5000;
     private AnimationDrawable animationDrawable;
     private  MediaPlayer mediaPlayer;
-    private VideoView container;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-//        ImageView container = findViewById(R.id.container);
-//        container.setBackgroundResource(R.drawable.splash_animation);
-//        animationDrawable = (AnimationDrawable) container.getBackground();
+        ImageView container = findViewById(R.id.container);
+        container.setBackgroundResource(R.drawable.splash_animation);
+        animationDrawable = (AnimationDrawable) container.getBackground();
 
-        container = findViewById(R.id.container);
-        container.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.logo_animation);
         try{
             mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.splash_screen_audio);
-            container.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.logo_animation);
-            container.start();
             mediaPlayer.start();
-            container.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    mp.setLooping(true);
-                }
-            });
-
-
-            checkAnimationStatus(TIME_INTERVAL, animationDrawable);
-
 
         }catch(Exception e){
             e.printStackTrace();
@@ -50,27 +34,25 @@ public class SplashScreenActivity extends AppCompatActivity {
             startActivity(i);
         }
 
-
-
     }
-//    @Override
-//    protected void onResume(){
-//        super.onResume();
-//
-//        try{
-//            animationDrawable.start();
-//            checkAnimationStatus(TIME_INTERVAL, animationDrawable);
-//        }
-//        catch(Exception e){
-//            e.printStackTrace();
-//
-//            Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
-//            startActivity(i);
-//
-//            // close this activity
-//            finish();
-//        }
-//    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        try{
+            animationDrawable.start();
+            checkAnimationStatus(TIME_INTERVAL, animationDrawable);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+
+            Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(i);
+
+            // close this activity
+            finish();
+        }
+    }
 
     private void checkAnimationStatus(final int time, AnimationDrawable animationDrawable) {
 
@@ -79,7 +61,7 @@ public class SplashScreenActivity extends AppCompatActivity {
             @Override
             public void run() {
                 currentTime += 1;
-                container.pause();
+
                 mediaPlayer.pause();
                 mediaPlayer.release();
                 mediaPlayer = null;
